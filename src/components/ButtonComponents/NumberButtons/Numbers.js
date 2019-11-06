79861da3fb8429d3462bd679c02000c3e6834cf8
occludedarp <cmrnmr@gmail.com>
2019-11-06 15:36:37 -0500

    display and buttons appearing could not finish click events

--- src/components/ButtonComponents/NumberButtons/Numbers.js
+++ src/components/ButtonComponents/NumberButtons/Numbers.js
@@ -1,4 +1,7 @@
-import React from "react";
+import React, {useState} from "react";
+import { numbers } from "../../../data";
+import NumberButton from "./NumberButton";
+
 
 //import any components needed
 // example of import from data.js. Note all the ../   This is how we move through folders. 
@@ -8,12 +11,14 @@ import { numbers } from '../../../data'
 //Import your array data to from the provided data file
 
 const Numbers = () => {
-  // STEP 2 - add the imported data to state
+  const [numberState, setNumberState]= useState(numbers);
   return (
     <div>
-      {/* STEP 3 - Use .map() to iterate over your array data and return a button
-       component matching the name on the provided file. Pass
-       it any props needed by the child component*/}
+      {numberState.map((number, index) => (
+        <NumberButton digitIs={number} updateDigitTo={setNumberState} key={index}/>
+      ))}
     </div>
   );
 };
+
+export default Numbers;
